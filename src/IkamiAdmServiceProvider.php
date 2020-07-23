b0a6a30dfafcd0d6395d81d16ca1ce10c576be15
Tetra Widianto <tetraw86@gmail.com>
2020-07-23 13:38:11 +0700

    initial commit

---
+++ src/IkamiAdmServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace IkamiAdm;
+
+use Illuminate\Support\ServiceProvider;
+
+class IkamiAdmServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // 
+    }
+
+    public function boot()
+    {
+        // 
+    }
+}
